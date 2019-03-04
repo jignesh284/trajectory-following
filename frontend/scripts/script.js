b8fbf78fd383d039d00c9d6879421bf23638414f
Jignesh Modi <jigneshmodi.developer@gmail.com>
2019-03-03 22:11:28 -0800

    Continious Line drawer added

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,34 +1,82 @@
-var game = $(".game");
+var game = $(".canvas-wrapper");
 var display = $(".display");
 var gameSection = $(".game-section");
 var end = $(".btn-end");
 var isMouseDown = false;
 
-game.mousemove(mouseMovementHandler);
-game.mousedown(function(event) {
-  isMouseDown = true;
-});
+/*********************** CANVAS ***************************** */
+var canvasHeight = game.height();
+var canvasWidth = game.width();
+game.append('<canvas id="game-canvas" width="'+canvasWidth+'" height="'+canvasHeight+'"></canvas>');
+var canvas = document.getElementById("game-canvas");
+console.log(canvas);
+var ctx = canvas.getContext("2d");
+var canvasOffset = getOffset(canvas);
+var offsetX = canvasOffset.x;
+var offsetY = canvasOffset.y;
+var startX;
+var startY;
+var isDown = false;
 
-game.mouseup(mouseUpHandler);
-
-end.click(endSession);
-
-function mouseUpHandler(event) {
-  console.log("mouse up");
-  isMouseDown = false;
+function drawLine(x, y) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(x, y);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "red";
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
 }
 
-function mouseMovementHandler(event) {
-  if( isMouseDown ) {
-    let shift = 5;
-    let x = event.pageX - shift;
-    let y = event.pageY - shift;
-    display.html(' (  X :: <font color="white">'+x+'</font> , Y :: <font color="white">'+y+'</font> )');
-    gameSection.append('<div class="dot" style="left: '+ x +'px; top: '+ y +'px;"></div>');
-  }
+canvas.onmousedown = function (e)  {
+    ctx.save();
+    e.preventDefault();
+    e.stopPropagation();
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+    console.log(startX, startY);
+    isDown = true;
 }
+
+canvas.onmousemove = function (e)  {
+    if (!isDown) {
+        return;
+    }
+    e.preventDefault();
+    e.stopPropagation();
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
+    display.html(' (  X :: <font color="white">'+mouseX+'</font> , Y :: <font color="white">'+ parseInt( canvasHeight - mouseY) +'</font> )');
+    drawLine(mouseX, mouseY);
+    startX = mouseX;
+    startY = mouseY;
+}
+
+canvas.onmouseup = function (e)  {
+    if (!isDown) {
+        return;
+    }
+    e.preventDefault();
+    e.stopPropagation();
+    isDown = false;
+}
+
+function getOffset(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while (element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    console.log(xPosition + " :: "+ yPosition);
+    return { x: xPosition, y: yPosition };
+}
+
+end.on('click', endSession);
 
 function endSession() {
-   $(".dot").remove();
-   display.html("");
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 }
